0113d4e35734c0fb8165c99e28189f32fe9478ab
sm1267 <94132306+sm1267@users.noreply.github.com>
2024-03-19 05:47:03 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,57 +1,226 @@
 #include <iostream>
-#include <array>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <algorithm>
 
-#include <Helper.h>
+
+
+void quiz() {
+    std::cout << " ___       ________  ________  ________          ________  ___  ___  ___  ________     " << std::endl;
+    std::cout << "|\\  \\     |\\   __  \\|\\   ____\\|\\   __  \\        |\\   __  \\|\\  \\|\\  \\|\\  \\|\\_____  \\    " << std::endl;
+    std::cout << "\\ \\  \\    \\ \\  \\|\\  \\ \\  \\___|\\ \\  \\|\\  \\       \\ \\  \\|\\  \\ \\  \\\\\\  \\ \\  \\\\|___/  /|   " << std::endl;
+    std::cout << " \\ \\  \\    \\ \\  \\\\\\  \\ \\  \\  __\\ \\  \\\\\\  \\       \\ \\  \\\\\\  \\ \\  \\\\\\  \\ \\  \\   /  / /   " << std::endl;
+    std::cout << "  \\ \\  \\____\\ \\  \\\\\\  \\ \\  \\|\\  \\ \\  \\\\\\  \\       \\ \\  \\\\\\  \\ \\  \\\\\\  \\ \\  \\ /  /_/__  " << std::endl;
+    std::cout << "   \\ \\_______\\ \\_______\\ \\_______\\ \\_______\\       \\ \\_____  \\ \\_______\\ \\__\\\\________\\" << std::endl;
+    std::cout << "    \\|_______|\\|_______|\\|_______|\\|_______|        \\|___| \\__\\|_______|\\|__|\\|_______|" << std::endl;
+    std::cout << "                                                          \\|__|                        " << std::endl;
+    std::cout << "                                                                                       " << std::endl;
+
+}
+
+
+
+class Jucator {
+private:
+    std::string Nume;
+    int Highscore;
+public:
+    Jucator(const std::string& nume, int highscore) : Nume(nume), Highscore(highscore) {}
+
+    Jucator(const Jucator& other) : Nume{ other.Nume }, Highscore{ other.Highscore } {}
+
+    const std::string& getNume() const {
+        return Nume;
+    }
+
+    void setNume(const std::string& nume) {
+        Nume = nume;
+    }
+
+    int getHighscore() const {
+        return Highscore;
+    }
+
+    void setHighscore(int highscore) {
+        Highscore = highscore;
+    }
+
+    virtual ~Jucator() {
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Jucator& jucator) {
+        os << "Nume: " << jucator.Nume << std::endl << "Scor: " << jucator.Highscore << std::endl;
+        return os;
+    }
+};
+
+class Nivel {
+private:
+    int Lungime;
+    bool Rezolvat;
+    std::string Nume;
+public:
+    void printBox(int l) {
+        std::string separator = "-";
+        std::string padding = "|";
+        std::string text = "|";
+        for (int j = 1; j <= l; ++j) {
+            separator += "------";
+            padding += "     |";
+            char value = '?';
+            text += "  ";
+            text += value;
+            text += "  |";
+        }
+        std::cout << separator << std::endl;
+        std::cout << padding << std::endl;
+        std::cout << text << std::endl;
+        std::cout << padding << std::endl;
+        std::cout << separator << std::endl;
+    }
+
+    void printLogo(std::string nume) {
+        nume = nume + ".txt";
+        std::fstream myFile;
+        myFile.open(nume, std::ios::in);
+        if (myFile.is_open()) {
+            std::string line;
+            while (getline(myFile, line)) {
+                std::cout << line << std::endl;
+            }
+        }
+        myFile.close();
+        std::cout << std::endl;
+    }
+
+    int getLungime() const {
+        return Lungime;
+    }
+
+    void setLungime(int lungime) {
+        Lungime = lungime;
+    }
+
+    const std::string& getNume() const {
+        return Nume;
+    }
+
+    void setNume(const std::string& nume) {
+        Nume = nume;
+    }
+
+    bool isRezolvat() const {
+        return Rezolvat;
+    }
+
+    void setRezolvat(bool rezolvat) {
+        Rezolvat = rezolvat;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Nivel& nivel) {
+        os << "Lungime: " << nivel.Lungime << " Rezolvat: " << nivel.Rezolvat << " Nume: " << nivel.Nume;
+        return os;
+    }
+};
+
+class Leaderboard {
+private:
+    std::vector<Jucator> players;
+
+public:
+    void addPlayer(const std::string& name, int score) {
+        players.push_back({ name, score });
+        sortPlayers();
+    }
+
+    void sortPlayers() {
+        std::sort(players.begin(), players.end(), [](const Jucator& a, const Jucator& b) {
+            return a.getHighscore() > b.getHighscore();  // Sort in descending order
+            });
+    }
+
+    void displayLeaderboard() {
+        std::cout << "Leaderboard:\n";
+        for (size_t i = 0; i < players.size(); ++i) {
+            std::cout << i + 1 << ". " << players[i].getNume() << " - " << players[i].getHighscore() << "\n";
+        }
+    }
+
+};
+
+Nivel Level[21];
 
 int main() {
-    std::cout << "Hello, world!\n";
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
+    quiz();
+
+    std::string nume;
+    std::cout << "Nume Jucator:" << std::endl;
+    std::getline(std::cin, nume);
+    Jucator Player(nume, 0);
+
+    std::string input;
+
+    std::string line;
+    std::ifstream file("nivele.txt");
+    int i = 1;
+    if (file.is_open()) {
+        while (getline(file, line)) {
+            int len = line.size();
+            Level[i].setLungime(len);
+            Level[i].setNume(line);
+            i++;
+        }
+        file.close();
     }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
+    
+    bool correct = true;
+
+    srand(time(0));
+    int v;
+    while(correct){
+        do {
+            v = rand() % 20 + 1;
+            while(Level[v].isRezolvat() == true) {
+                v = rand() % 20 + 1;
+            }
+            std::string answer = Level[v].getNume();
+            std::cout << "Numiti Brand-ul sau apasati tasta Q pentru a iesi: \n" << std::endl;
+            Level[v].printLogo(answer);
+            Level[v].printBox(Level[v].getLungime());
+            std::getline(std::cin, input);
+            std::transform(input.begin(), input.end(), input.begin(), ::tolower);
+            if (input == answer) {
+                int hs = Player.getHighscore();
+                hs++;
+                Player.setHighscore(hs);
+                Level[v].setRezolvat(true);
+                system("cls");
+            }
+            else {
+                correct = false;
+                break;
+            }
+            if (Player.getHighscore() == 20)
+                break;
+        } while (input != "q");
+        if (input == "q")
+        {
+            std::cout << "Quit game" << std::endl;
+            break;
+        }
+        if (Player.getHighscore() == 20)
+            break;
     }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
-    ///////////////////////////////////////////////////////////////////////////
-    ///                Exemplu de utilizare cod generat                     ///
-    ///////////////////////////////////////////////////////////////////////////
-    Helper helper;
-    helper.help();
-    ///////////////////////////////////////////////////////////////////////////
+
+    Leaderboard l;
+    l.addPlayer(Player.getNume(), Player.getHighscore());
+
+    if (Player.getHighscore() == 20)
+        std::cout << "Ai castigat! (20/20)" << std::endl;
+    else
+        operator<<(std::cout, Player);
+
+    l.displayLeaderboard();
     return 0;
 }
